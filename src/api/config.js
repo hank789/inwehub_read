e64838b6ae810d672ec4cbaa608b2751498dd65f
hank <hank.huiwang@gmail.com>
2017-04-12 14:09:49 +0800

    去除多余的空格

--- src/api/config.js
+++ src/api/config.js
@@ -11,7 +11,7 @@ const config = {
   TOPIC_API: '/topic',
   TOPIC_NEW_COUNT_API: '/topic/newCount',
   WECHAT_SIGNATURE_URL: '/wechatsdk/getSignature',
-  TITLE: 'Readhub',
+  TITLE: 'Inwehub',
   WX_APPID: 'wx9dfc5d4bce5da3f5',
   SENTRY_DSN: 'http://18e661e59787435baafca244af299816@sentry.nocode.mobi/5'
 }
