6400dc0b1a7bbcf4a6f4f5bb3e4da2207cb8fa3f
hank <hank.huiwang@gmail.com>
2017-04-17 13:50:40 +0800

    add ga

--- src/main.js
+++ src/main.js
@@ -5,12 +5,20 @@ import { Button, Row, Col } from 'element-ui'
 import App from './App'
 import store from './store'
 import router from './router'
+import VueAnalytics from 'vue-ua'
 
 Vue.config.productionTip = false
 
 Vue.use(Button)
 Vue.use(Row)
 Vue.use(Col)
+Vue.use(VueAnalytics, {
+  appName: 'inwehub', // Mandatory
+  appVersion: 'V1', // Mandatory
+  trackingId: 'UA-97476315-1', // Mandatory
+  debug: true, // Whether or not display console logs debugs (optional)
+  vueRouter: router // Pass the router instance to automatically sync with router (optional)
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
